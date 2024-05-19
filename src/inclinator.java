b61138ef14f64c54512d67a5138e34985e35ccc3
Ivana11235 <165697511+Ivana11235@users.noreply.github.com>
2024-05-19 19:03:38 -0400

    Update inclinator.java

--- src/inclinator.java
+++ src/inclinator.java
@@ -3,11 +3,16 @@ import java.io.*;
 
 public class inclinator {
 
+
+
+
     public ArrayList<Nation> nations;
+     
+
     private ArrayList<String> cs=new ArrayList<String>(){{
-        ArrayList<String> cs;
-        add("cyber");
-        add("internet");}};
+       ArrayList<String> cs;
+       add("cyber");
+       add("internet");}};
     private ArrayList<String> hr=new ArrayList<String>(){{
 
         add("human rights");
@@ -16,8 +21,8 @@ public class inclinator {
     }};
     private ArrayList<String> wr=new ArrayList<String>(){{
 
-        add("women");
-        add("female");
+       add("women");
+       add("female");
     }};
     private ArrayList<String> g=new ArrayList<String>(){{
         //ArrayList<String> g=new ArrayList<>();
@@ -30,6 +35,18 @@ public class inclinator {
         add("climate");
     }};
 
+    private ArrayList<String> o=new ArrayList<String>(){{
+
+        add("charter");
+    }};
+
+    private ArrayList<String> me=new ArrayList<String>(){{
+
+        add("Gaza");
+        add("Syria");
+        add("Middle East");
+    }};
+
     private HashMap<String, ArrayList<String>> key=new HashMap (){{
 
         put("cs",cs);
@@ -37,31 +54,33 @@ public class inclinator {
         put("wr",wr);
         put("g",g);
         put("e",e);
+        put("o",o);
+        put("me",me);
     }};
 
-
-
-    private ArrayList<Float> Inclination=new ArrayList<>();
+    
 
     public void inclinate(String keyword) throws FileNotFoundException {
 
 
 
         int fileCount=0;
-        File[] fileArray=new File("!!!!!!!input!!!").listFiles();
+        File[] fileArray=new File("un").listFiles();
 
         for(File f: fileArray){
+            ArrayList list =key.get(keyword);
             boolean checkFile=false;
-            Scanner s=new Scanner(f); // to read the files
+            Scanner s=new Scanner(f);
+            System.out.println(s.nextLine());
             for(int i=0;i<10;i++){
                 String line=s.nextLine();
-                ArrayList list =key.get(keyword);
+
                 for (Object word: list){
                     if (line.contains(word.toString())){
                         checkFile=true;
                     }
                 }}
-            if (checkFile==true){
+            if (checkFile){
                 fileCount+=1;
                 while (s.hasNextLine()){
                     String line=s.nextLine();
@@ -75,8 +94,8 @@ public class inclinator {
             }s.close();
         }
         for (Nation n: nations){
-            n.inclination/=fileCount;}
+            n.inclination/=fileCount;
+            System.out.println(n.getName()+" "+n.inclination);
+        }
 
-    }
-
-}
+}}
